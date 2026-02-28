9a5a2f2fb8730f8ea5fc3b01ed057257d12feefc
Shah Meer Aftab <106464084+ShahmeerAftab@users.noreply.github.com>
2026-03-01 03:14:14 +0500

    Update User.js

--- New folder/User.js
+++ New folder/User.js
@@ -13,17 +13,18 @@ const userSchema = new mongoose.Schema({
   subscriptionPlan: { type: String, enum: ["free", "pro"], default: "free" }
 }, { timestamps: true });
 
-// Password hashing before saving
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// ⭐ YE METHOD MISSING THA: Password compare karne ke liye
+
 userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
+
 export default User;
